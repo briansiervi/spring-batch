43ff3a15449779f60c1021c4b14eac77b789b4f5
briansiervi <brianmazini@gmail.com>
2024-10-26 09:21:32 -0300

    feat: criacao do springbatch01

---
+++ springbatch01/src/main/java/br/com/briansiervi/springbatch01/Springbatch01Application.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.springbatch01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbatch01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbatch01Application.class, args);
+	}
+
+}
---
+++ springbatch01/src/test/java/br/com/briansiervi/springbatch01/Springbatch01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.springbatch01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbatch01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
