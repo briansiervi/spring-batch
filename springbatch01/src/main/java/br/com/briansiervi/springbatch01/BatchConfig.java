e5955cade59b3de53703e2a3b04670cb99464095
briansiervi <brianmazini@gmail.com>
2024-10-26 21:27:33 -0300

    feat(config): configurações do vscode + script de limpeza de tabelas

--- springbatch01/src/main/java/br/com/briansiervi/springbatch01/BatchConfig.java
+++ springbatch01/src/main/java/br/com/briansiervi/springbatch01/BatchConfig.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -27,6 +28,9 @@ public class BatchConfig {
     return jobBuilderFactory
         .get("imprimeOlaJob")
         .start(imprimeOlaStep())
+        .incrementer(new RunIdIncrementer()) // nota: se utilizado, impede que o job retome o estado anterior à uma
+                                             // interrupção, porém para testes, faz com que não tenhamos que ficar
+                                             // limpando os metadados gerados
         .build();
   }
 
