c50677dbf62c1fa78c6a485f5eb3e16b2d93215c
briansiervi <brianmazini@gmail.com>
2024-10-26 22:51:46 -0300

    feat: parâmetros de execução

--- springbatch01/src/main/java/br/com/briansiervi/springbatch01/BatchConfig.java
+++ springbatch01/src/main/java/br/com/briansiervi/springbatch01/BatchConfig.java
@@ -6,11 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -37,22 +39,15 @@ public class BatchConfig {
   private Step imprimeOlaStep() {
     return stepBuilderFactory
         .get("imprimeOlaStep")
-        .tasklet(tasklet())
+        .tasklet(imprimeOlaTasklet(null))
         .build();
   }
 
-  private Tasklet tasklet() {
-    // Tasklet tasklet = new Tasklet() {
-    // @Override
-    // public RepeatStatus execute(StepContribution contribution, ChunkContext
-    // chunkContext) throws Exception {
-    // System.out.println("Olá, mundo!");
-    // return RepeatStatus.FINISHED;
-    // }
-    // };
-
+  @Bean
+  @StepScope
+  public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome) {
     Tasklet tasklet = (StepContribution contribution, ChunkContext chunkContext) -> {
-      System.out.println("Olá, mundo!");
+      System.out.println(String.format("Olá, %s!", nome));
       return RepeatStatus.FINISHED;
     };
 
