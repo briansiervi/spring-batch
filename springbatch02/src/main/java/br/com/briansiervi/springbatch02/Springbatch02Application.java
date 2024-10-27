0a419d9a213172a7364ad699af52c31513cb6020
briansiervi <brianmazini@gmail.com>
2024-10-26 23:19:37 -0300

    feat: projeto 02 - par ou ímpar

---
+++ springbatch02/src/main/java/br/com/briansiervi/springbatch02/Springbatch02Application.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.springbatch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbatch02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbatch02Application.class, args);
+	}
+
+}
---
+++ springbatch02/src/test/java/br/com/briansiervi/springbatch02/Springbatch02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.springbatch02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbatch02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
