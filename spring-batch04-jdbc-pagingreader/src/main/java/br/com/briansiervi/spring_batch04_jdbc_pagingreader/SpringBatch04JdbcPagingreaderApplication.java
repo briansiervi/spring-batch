9d322d9d36ff7c45856c25991e14aafb8aa977ae
briansiervi <brianmazini@gmail.com>
2024-11-19 08:32:18 -0300

    feat(spring_batch04_jdbc_pagingreader): commit inicial do spring_batch04_jdbc_pagingreader

---
+++ spring-batch04-jdbc-pagingreader/src/main/java/br/com/briansiervi/spring_batch04_jdbc_pagingreader/SpringBatch04JdbcPagingreaderApplication.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.spring_batch04_jdbc_pagingreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch04JdbcPagingreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch04JdbcPagingreaderApplication.class, args);
+	}
+
+}
---
+++ spring-batch04-jdbc-pagingreader/src/test/java/br/com/briansiervi/spring_batch04_jdbc_pagingreader/SpringBatch04JdbcPagingreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.spring_batch04_jdbc_pagingreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch04JdbcPagingreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
