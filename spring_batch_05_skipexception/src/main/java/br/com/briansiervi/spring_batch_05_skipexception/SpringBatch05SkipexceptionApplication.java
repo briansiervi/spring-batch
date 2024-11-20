202512695dd2af4272fab026481575e5c2f1edf4
briansiervi <brianmazini@gmail.com>
2024-11-20 12:30:47 -0300

    feat(spring_batch_05_skipexception): commit inicial

---
+++ spring_batch_05_skipexception/src/main/java/br/com/briansiervi/spring_batch_05_skipexception/SpringBatch05SkipexceptionApplication.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.spring_batch_05_skipexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch05SkipexceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch05SkipexceptionApplication.class, args);
+	}
+
+}
---
+++ spring_batch_05_skipexception/src/test/java/br/com/briansiervi/spring_batch_05_skipexception/SpringBatch05SkipexceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.spring_batch_05_skipexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch05SkipexceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
