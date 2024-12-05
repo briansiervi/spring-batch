4a703c9dab1ca30c70156ed281d26c2cd1f8728d
briansiervi <brianmazini@gmail.com>
2024-12-05 08:58:11 -0300

    feat(spring_batch_06_largurafixa): commit inicial

---
+++ spring_batch_06_largurafixa/src/main/java/br/com/briansiervi/spring_batch_06_largurafixa/SpringBatch06LargurafixaApplication.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.spring_batch_06_largurafixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch06LargurafixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch06LargurafixaApplication.class, args);
+	}
+
+}
---
+++ spring_batch_06_largurafixa/src/test/java/br/com/briansiervi/spring_batch_06_largurafixa/SpringBatch06LargurafixaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.spring_batch_06_largurafixa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch06LargurafixaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
