ddcab9c611d4562b1f3cca1b8d934389560d4c4d
briansiervi <brianmazini@gmail.com>
2024-12-29 11:01:30 -0300

    feat(spring_batch_07_arquivo_multiplos_formatos): commit inicial

---
+++ spring_batch_07_arquivo_multiplos_formatos/src/main/java/br/com/briansiervi/spring_batch_07_arquivo_multiplos_formatos/SpringBatch07ArquivoMultiplosFormatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.spring_batch_07_arquivo_multiplos_formatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch07ArquivoMultiplosFormatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch07ArquivoMultiplosFormatosApplication.class, args);
+	}
+
+}
---
+++ spring_batch_07_arquivo_multiplos_formatos/src/test/java/br/com/briansiervi/spring_batch_07_arquivo_multiplos_formatos/SpringBatch07ArquivoMultiplosFormatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.briansiervi.spring_batch_07_arquivo_multiplos_formatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch07ArquivoMultiplosFormatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
