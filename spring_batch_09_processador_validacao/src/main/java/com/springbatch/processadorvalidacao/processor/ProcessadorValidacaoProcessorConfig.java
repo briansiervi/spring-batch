2e324b3058abc4b39f7fdbae8c8d46c9c20249bc
briansiervi <brianmazini@gmail.com>
2025-01-12 10:55:16 -0300

    processador composto finalizado "CompositeItemProcessorBuilder"

--- spring_batch_09_processador_validacao/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
+++ spring_batch_09_processador_validacao/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
@@ -1,6 +1,14 @@
 package com.springbatch.processadorvalidacao.processor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.builder.CompositeItemProcessorBuilder;
+import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidationException;
+import org.springframework.batch.item.validator.Validator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +16,40 @@ import com.springbatch.processadorvalidacao.dominio.Cliente;
 
 @Configuration
 public class ProcessadorValidacaoProcessorConfig {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private Set<String> emails = new HashSet();
+
 	@Bean
-	public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() {
-		// TODO: Implementar a lógica aqui...
-		return cliente -> cliente;
+	public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() throws Exception {
+		return new CompositeItemProcessorBuilder<Cliente, Cliente>()
+				.delegates(beanValidatingProcessor(), emailValidatingProcessor())
+				.build();
+	}
+
+	private BeanValidatingItemProcessor<Cliente> beanValidatingProcessor() throws Exception {
+		BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
+		processor.setFilter(true);
+		processor.afterPropertiesSet();
+		return processor;
+	}
+
+	private ValidatingItemProcessor<Cliente> emailValidatingProcessor() {
+		ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>();
+		processor.setValidator(validator());
+		processor.setFilter(true);
+		return processor;
+	}
+
+	private Validator<Cliente> validator() {
+		return new Validator<Cliente>() {
+			@Override
+			public void validate(@SuppressWarnings("null") Cliente cliente) throws ValidationException {
+				if (emails.contains(cliente.getEmail()))
+					throw new ValidationException(String.format("O cliente %s já foi processado!", cliente.getEmail()));
+
+				emails.add(cliente.getEmail());
+			}
+
+		};
 	}
 }
