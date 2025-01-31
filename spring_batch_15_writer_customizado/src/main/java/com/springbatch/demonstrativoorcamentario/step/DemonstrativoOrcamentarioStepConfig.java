a21de90ef94df28439c1626cf3e125e85dfe2bdb
briansiervi <brianmazini@gmail.com>
2025-01-31 09:07:19 -0300

    feat(spring_batch_15_writer_customizado): escrita num único arquivo

--- spring_batch_15_writer_customizado/src/main/java/com/springbatch/demonstrativoorcamentario/step/DemonstrativoOrcamentarioStepConfig.java
+++ spring_batch_15_writer_customizado/src/main/java/com/springbatch/demonstrativoorcamentario/step/DemonstrativoOrcamentarioStepConfig.java
@@ -9,24 +9,27 @@ import org.springframework.context.annotation.Configuration;
 
 import com.springbatch.demonstrativoorcamentario.dominio.GrupoLancamento;
 import com.springbatch.demonstrativoorcamentario.reader.GrupoLancamentoReader;
+import com.springbatch.demonstrativoorcamentario.writer.DemonstrativoOrcamentarioRodape;
 
 @Configuration
 public class DemonstrativoOrcamentarioStepConfig {
 	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
-	
+
 	@Bean
 	public Step demonstrativoOrcamentarioStep(
 			// Esse aqui lê dos arquivos
-			//MultiResourceItemReader<GrupoLancamento> demonstrativoOrcamentarioReader,
+			// MultiResourceItemReader<GrupoLancamento> demonstrativoOrcamentarioReader,
 			// Esse aqui lê do banco de dados
 			GrupoLancamentoReader demonstrativoOrcamentarioReader,
-			ItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter) {
+			ItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter,
+			DemonstrativoOrcamentarioRodape rodapeCallbackListener) {
 		return stepBuilderFactory
 				.get("demonstrativoOrcamentarioStep")
-				.<GrupoLancamento,GrupoLancamento>chunk(100)
+				.<GrupoLancamento, GrupoLancamento>chunk(100)
 				.reader(demonstrativoOrcamentarioReader)
 				.writer(demonstrativoOrcamentarioWriter)
+				.listener(rodapeCallbackListener)
 				.build();
 	}
 }
--- spring_batch_15_writer_customizado/src/main/java/com/springbatch/demonstrativoorcamentario/writer/DemonstrativoOrcamentarioWriterConfig.java
+++ spring_batch_15_writer_customizado/src/main/java/com/springbatch/demonstrativoorcamentario/writer/DemonstrativoOrcamentarioWriterConfig.java
@@ -1,12 +1,21 @@
 package com.springbatch.demonstrativoorcamentario.writer;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.LineAggregator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import com.springbatch.demonstrativoorcamentario.dominio.GrupoLancamento;
 import com.springbatch.demonstrativoorcamentario.dominio.Lancamento;
@@ -14,31 +23,62 @@ import com.springbatch.demonstrativoorcamentario.dominio.Lancamento;
 @Configuration
 public class DemonstrativoOrcamentarioWriterConfig {
 	@Bean
-	public ItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter() {
-		return itens -> {
-			System.out.println("\n");
-			System.out.println(String.format("SISTEMA INTEGRADO: XPTO \t\t\t\t DATA: %s", new SimpleDateFormat("dd/MM/yyyy").format(new Date())));
-			System.out.println(String.format("MÓDULO: ORÇAMENTO \t\t\t\t\t HORA: %s", new SimpleDateFormat("HH:MM").format(new Date())));
-			System.out.println(String.format("\t\t\tDEMONSTRATIVO ORCAMENTARIO"));
-			System.out.println(String.format("----------------------------------------------------------------------------"));
-			System.out.println(String.format("CODIGO NOME VALOR"));
-			System.out.println(String.format("\t Data Descricao Valor"));
-			System.out.println(String.format("----------------------------------------------------------------------------"));
-			
-			Double totalGeral = 0.0;
-			for (GrupoLancamento grupo : itens) {
-				System.out.println(String.format("[%d] %s - %s", grupo.getCodigoNaturezaDespesa(),
-						grupo.getDescricaoNaturezaDespesa(),
-						NumberFormat.getCurrencyInstance().format(grupo.getTotal())));
-				totalGeral += grupo.getTotal();
-				for (Lancamento lancamento : grupo.getLancamentos()) {
-					System.out.println(String.format("\t [%s] %s - %s", new SimpleDateFormat("dd/MM/yyyy").format(lancamento.getData()), lancamento.getDescricao(),
-							NumberFormat.getCurrencyInstance().format(lancamento.getValor())));
-				}
+	@StepScope
+	public FlatFileItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter(
+			@Value("#{jobParameters['demonstrativoOrcamentario']}") Resource demonstrativoOrcamentario,
+			DemonstrativoOrcamentarioRodape rodapeCallback) {
+		return new FlatFileItemWriterBuilder<GrupoLancamento>()
+				.name("demonstrativoOrcamentarioWriter")
+				.resource(demonstrativoOrcamentario)
+				.lineAggregator(lineAggregator())
+				.headerCallback(cabecalhoCallback())
+				.footerCallback(rodapeCallback)
+				.build();
+	}
+
+	private FlatFileHeaderCallback cabecalhoCallback() {
+		return new FlatFileHeaderCallback() {
+			@Override
+			public void writeHeader(@SuppressWarnings("null") Writer writer) throws IOException {
+				writer.append("\n");
+				writer.append(String.format("SISTEMA INTEGRADO: XPTO \t\t\t\t DATA: %s\n",
+						new SimpleDateFormat("dd/MM/yyyy").format(new Date())));
+				writer.append(
+						String.format("MÓDULO: ORÇAMENTO \t\t\t\t\t\t\t HORA: %s\n" + //
+								"", new SimpleDateFormat("HH:MM").format(new Date())));
+				writer.append(String.format("\t\t\t\tDEMONSTRATIVO ORCAMENTARIO\n"));
+				writer.append(String.format("----------------------------------------------------------------------------\n"));
+				writer.append(String.format("CODIGO NOME VALOR\n"));
+				writer.append(String.format("\t Data Descricao Valor\n"));
+				writer.append(String.format("----------------------------------------------------------------------------\n"));
 			}
-			System.out.println("\n");
-			System.out.println(String.format("\t\t\t\t\t\t\t  Total: %s", NumberFormat.getCurrencyInstance().format(totalGeral)));
-			System.out.println(String.format("\t\t\t\t\t\t\t  Código de Autenticação: %s", "fkyew6868fewjfhjjewf"));
+		};
+	}
+
+	private LineAggregator<GrupoLancamento> lineAggregator() {
+		return new LineAggregator<GrupoLancamento>() {
+
+			@SuppressWarnings("null")
+			@Override
+			public String aggregate(GrupoLancamento grupoLancamento) {
+				String formatoGrupoLancamento = String.format("[%d] %s - %s\n",
+						grupoLancamento.getCodigoNaturezaDespesa(),
+						grupoLancamento.getDescricaoNaturezaDespesa(),
+						NumberFormat.getCurrencyInstance().format(grupoLancamento.getTotal()));
+
+				StringBuilder strBuilder = new StringBuilder();
+
+				for (Lancamento lancamento : grupoLancamento.getLancamentos()) {
+					strBuilder.append(
+							String.format("\t [%s] %s - %s\n", new SimpleDateFormat("dd/MM/yyyy").format(lancamento.getData()),
+									lancamento.getDescricao(),
+									NumberFormat.getCurrencyInstance().format(lancamento.getValor())));
+				}
+
+				String formatoLancamento = strBuilder.toString();
+				return formatoGrupoLancamento + formatoLancamento;
+			}
+
 		};
 	}
 }
