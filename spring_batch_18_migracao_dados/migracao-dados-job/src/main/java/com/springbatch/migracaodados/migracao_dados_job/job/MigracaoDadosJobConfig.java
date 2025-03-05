e89365db1d6fc1846591199af0d8713917daf598
briansiervi <brianmazini@gmail.com>
2025-03-04 22:44:21 -0300

    feat(spring_batch_18_migracao_dados): job, step e dtos configurados

---
+++ spring_batch_18_migracao_dados/migracao-dados-job/src/main/java/com/springbatch/migracaodados/migracao_dados_job/job/MigracaoDadosJobConfig.java
@@ -0,0 +1,27 @@
+package com.springbatch.migracaodados.migracao_dados_job.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MigracaoDadosJobConfig {
+  @Autowired
+  JobBuilderFactory jobBuilderFactory;
+
+  @Bean
+  public Job migracaoDadosJob(
+      Step migrarPessoaStep,
+      Step migrarDadosBancariosStep) {
+    return jobBuilderFactory
+        .get("migracaoDadosJob")
+        .start(migrarPessoaStep)
+        .next(migrarDadosBancariosStep)
+        .incrementer(new RunIdIncrementer())
+        .build();
+  }
+}
---
+++ spring_batch_18_migracao_dados/migracao-dados-job/src/main/java/com/springbatch/migracaodados/migracao_dados_job/step/MigrarDadosBancariosStepConfig.java
@@ -0,0 +1,29 @@
+package com.springbatch.migracaodados.migracao_dados_job.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.migracaodados.migracao_dados_job.dominio.DadoBancario;
+
+@Configuration
+public class MigrarDadosBancariosStepConfig {
+  @Autowired
+  private StepBuilderFactory stepBuilderFactory;
+
+  @Bean
+  public Step migrarDadosBancariosStep(
+      ItemReader<DadoBancario> arquivoDadosBancariosReader,
+      ItemWriter<DadoBancario> bancoDadosBancariosWriter) {
+    return stepBuilderFactory
+        .get("migrarDadosBancariosStep")
+        .<DadoBancario, DadoBancario>chunk(1)
+        .reader(arquivoDadosBancariosReader)
+        .writer(bancoDadosBancariosWriter)
+        .build();
+  }
+}
