d90891ab7c91ea000df1cbe93a7b682dd2b2b6df
briansiervi <brianmazini@gmail.com>
2025-03-08 22:39:48 -0300

    feat(spring_batch_19_cartao_credito): commit inicial

---
+++ spring_batch_19_cartao_credito/src/main/java/com/springbatch/fatura_cartao_credito_job/FaturaCartaoCreditoJobApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.fatura_cartao_credito_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturaCartaoCreditoJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaturaCartaoCreditoJobApplication.class, args);
+	}
+
+}
