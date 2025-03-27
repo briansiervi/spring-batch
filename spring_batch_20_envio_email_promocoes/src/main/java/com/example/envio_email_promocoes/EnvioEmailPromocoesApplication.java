27c667c0ac035766643f481e1d4ef6d3a82173b9
briansiervi <brianmazini@gmail.com>
2025-03-27 08:43:14 -0300

    feat(spring_batch_20_envio_email_promocoes): commit inicial

---
+++ spring_batch_20_envio_email_promocoes/src/main/java/com/example/envio_email_promocoes/EnvioEmailPromocoesApplication.java
@@ -0,0 +1,13 @@
+package com.example.envio_email_promocoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvioEmailPromocoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvioEmailPromocoesApplication.class, args);
+	}
+
+}
---
+++ spring_batch_20_envio_email_promocoes/src/test/java/com/example/envio_email_promocoes/EnvioEmailPromocoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.envio_email_promocoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvioEmailPromocoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
